9980dc50fd616b5d63bb8b43f40eeae4672662ee
Dineshkumar Arunachalam <dineshkumar@ssdiblr.sharp.co.in>
2025-05-28 15:33:54 +0530

    updated

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const server =http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://dineshchitchat.netlify.app/chatpage',
+    origin: 'https://dineshchitchat.netlify.app',
     methods: ['GET', 'POST'],
     credentials: true
   }
